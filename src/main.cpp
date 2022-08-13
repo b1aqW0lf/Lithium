b246e62dcd31c0f50d7821d4baf7bda8c01cb334
b1aqW0lf <VectorSigma97@protonmail.com>
2022-08-13 02:16:59 -0400

    pixmap highdpi attribute to main

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,8 @@
 int main(int argc, char *argv[])
 {
 #if QT_VERSION < QT_VERSION_CHECK(6,0,0)
-    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
+    QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
 #endif
 
     QApplication app(argc, argv);
