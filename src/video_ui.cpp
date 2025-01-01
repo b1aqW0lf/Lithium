757704b1f296df69fd0266673d61fe1675d55ec8
b1aqW0lf <VectorSigma97@protonmail.com>
2025-01-01 18:09:03 -0500

    added missing header

--- src/video_ui.cpp
+++ src/video_ui.cpp
@@ -29,6 +29,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ******************************************************************************/
 
 
+#include <QAction>
 #include <QComboBox>
 #include <QDial>
 #include <QGroupBox>
