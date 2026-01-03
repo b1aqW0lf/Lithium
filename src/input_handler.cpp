dd14c204d4b91a976d174eff2c8ae40352d6c6e5
b1aqW0lf <VectorSigma97@protonmail.com>
2026-01-03 15:43:16 -0500

    experiment/removed redundant this pointer instances

--- src/input_handler.cpp
+++ src/input_handler.cpp
@@ -8,53 +8,53 @@ InputHandler::~InputHandler(){/*destructor*/}
 void InputHandler::send_input_selections_request()
 {
     //request the user input selections/parameters
-    Q_EMIT this->request_input_selections();
+    Q_EMIT request_input_selections();
 }
 
 void InputHandler::receive_input_video_selection(const QStringList &video_selection)
 {
-    this->list.video_selection = video_selection;
+    list.video_selection = video_selection;
 }
 
 void InputHandler::receive_input_crf_qscale_selection(const QStringList &crf_qscale_selection)
 {
-    this->list.crf_qscale_selection = crf_qscale_selection;
+    list.crf_qscale_selection = crf_qscale_selection;
 }
 
 void InputHandler::receive_video_avg_bitrate_selections(const QStringList &bitrate_selections)
 {
-    this->list.bitrate_selections = bitrate_selections;
+    list.bitrate_selections = bitrate_selections;
 }
 
 void InputHandler::receive_input_video_preset_selection(const QStringList &preset_selection)
 {
-    this->list.preset_selection = preset_selection;
+    list.preset_selection = preset_selection;
 }
 
 void InputHandler::receive_video_options_selections(const QStringList &options_selections)
 {
-    this->list.options_selections = options_selections;
+    list.options_selections = options_selections;
 }
 
 void InputHandler::receive_input_audio_selections(const QStringList &audio_selections)
 {
-    this->list.audio_selections = audio_selections;
+    list.audio_selections = audio_selections;
 }
 
 void InputHandler::send_received_selected_inputs()
 {
-    this->process_selected_inputs();
+    process_selected_inputs();
 }
 
 void InputHandler::process_selected_inputs()
 {
-    this->input_selections.clear();
+    input_selections.clear();
 
-    this->input_selections << this->list.video_selection
-                           << this->list.crf_qscale_selection
-                           << this->list.preset_selection
-                           << this->list.options_selections
-                           << this->list.audio_selections;
+    input_selections << list.video_selection
+                     << list.crf_qscale_selection
+                     << list.preset_selection
+                     << list.options_selections
+                     << list.audio_selections;
 
-    Q_EMIT this->send_selected_input_parameters(this->input_selections);
+    Q_EMIT send_selected_input_parameters(input_selections);
 }
