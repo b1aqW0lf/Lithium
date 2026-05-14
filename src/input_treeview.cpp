e16fcb30a0755fe84c9f8ccf4260cec4fa1ad803
b1aqW0lf <VectorSigma97@protonmail.com>
2026-05-14 15:37:36 -0400

    experiment/renamed receive_source_video_file parameter to video_file

--- src/input_treeview.cpp
+++ src/input_treeview.cpp
@@ -58,9 +58,9 @@ InputTreeView::~InputTreeView()
     delete ui;
 }
 
-void InputTreeView::receive_source_video_file(const QString &source_file)
+void InputTreeView::receive_source_video_file(const QString &video_file)
 {
-    this->source_file = source_file;
+    this->source_file = video_file;
 }
 
 void InputTreeView::current_process_mode(ProcessMode mode)
